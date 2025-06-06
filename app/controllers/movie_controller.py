51b1c949923674657368a35d0ac7c7c3a17bafdd
Pradip Waghela <pradip.waghela@tridiagonal.com>
2025-06-06 16:01:13 +0530

    add - assignment

--- app/controllers/movie_controller.py
+++ app/controllers/movie_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash 
 
 from app.forms import MovieForm
 from app.services import get_movieids, get_random_movie
@@ -16,27 +16,38 @@ class MovieController:
         Returns:
             render_template: Render home page
         """
-        form = MovieForm()
-        if form.validate_on_submit():
-            language = form.languages.data
-            geners = form.movie_geners.data
-            movie_ids = get_movieids(language, geners)
-            (
-                movie_name,
-                movie_lan,
-                movie_genre,
-                movie_creator,
-                movie_url,
-                movie_imdb_rating,
-            ) = get_random_movie(movie_ids)
-            
-            movie_details = {
-                "name": movie_name,
-                "geners": movie_genre,
-                "languages": movie_lan,
-                "director": movie_creator,
-                "rating": movie_imdb_rating,
-                "url": movie_url,
-            }
-            return render_template("movie.html", movie=movie_details)
+        try :
+            form = MovieForm()
+            if form.validate_on_submit():
+                language = form.languages.data
+                geners = form.movie_geners.data
+                movie_ids = get_movieids(language, geners)
+                (
+                    movie_name,
+                    movie_lan,
+                    movie_genre,
+                    movie_creator,
+                    movie_url,
+                    movie_imdb_rating,
+                ) = get_random_movie(movie_ids)
+                
+                movie_details = {
+                    "name": movie_name,
+                    "geners": movie_genre,
+                    "languages": movie_lan,
+                    "director": movie_creator,
+                    "rating": movie_imdb_rating,
+                    "url": movie_url,
+                }
+                return render_template("movie.html", movie=movie_details)
+        except TypeError :
+            print("No movie found for selected input")
+            flash("No Movie found for selected input")
+        except Exception as e :
+            print(f"Error occuer {e}")
+            flash("Error While suggesting movie Please Try Again")
+
         return render_template("index.html", form=form)
+            
+        
+            
