367d82bf5e72407ea99acba4086ac8b92bbdb070
Pradip Waghela <pradip.waghela@tridiagonal.com>
2025-06-25 23:28:50 +0530

    impro : config for CSRF add CSRF

--- app/extensions.py
+++ app/extensions.py
@@ -2,9 +2,10 @@ from  flask_pymongo import PyMongo
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-
+from flask_wtf import CSRFProtect
 
 mongo = PyMongo()
 login = LoginManager()
 migrate = Migrate()
 jwt = JWTManager()
+csrf = CSRFProtect()
--- config.py
+++ config.py
@@ -20,6 +20,9 @@ class Config:
     MONGO_URI = os.environ.get('MONGO_URI') or "mongodb://localhost:27017/movie_db"
     JWT_SECRET_KEY = "movie_secret"
     JWT_TOKEN_LOCATION = ['cookies']
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_CSRF_CHECK_FORM = True
+    
 
 
 class ProductionConfig(Config):
