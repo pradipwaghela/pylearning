1c781fb486910b5598da0b730ffeea35b4c1a82b
pradipOnOffice <pradip.waghela787@gmail.com>
2025-07-09 20:12:58 +0530

    bug : resolved callback error

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,9 +1,9 @@
-import request
+
 import logging
 
 from app.constants import User_Endpoints
 from app.services  import Auth
-from flask import  redirect, url_for
+from flask import  redirect, url_for, request
 from flask_jwt_extended import set_access_cookies
 
 def before_request():
@@ -18,25 +18,4 @@ def before_request():
 
 
 
-def unauthorized_callback(callback):
-    '''
-    If not a valid JWT token then redirect user to login page 
-    '''
-    try :
-        logging.debug("Not a valide JWT token please login back in ")
-        return redirect(url_for('user.login'))
-    except Exception as e :
-        logging.error("Error while redirecting user for re-login %s",e)
-        
-def refresh_token(response):
-    '''
-    Refresh JWT token if expired or going to expire 
-    '''
-    try:
-        access_token = Auth.refresh_expiring_jwts()
-        if access_token:
-            set_access_cookies(response, access_token)
-        return response
-    except (RuntimeError, KeyError) as e:
-        logging.error("Error while refreshing token %s ",e)
-        return response
+     
--- app/movie/routes/movie_bp.py
+++ app/movie/routes/movie_bp.py
@@ -4,7 +4,6 @@ movie_suggest -> Main blue print
 
 """
 from flask import Blueprint
-from app.middlewares import unauthorized_callback, refresh_token 
 from flask_jwt_extended import jwt_required
 
 from app.extensions import (jwt , csrf)
@@ -14,11 +13,33 @@ from app.services import Auth
 
 movie_suggest = Blueprint("movie_suggest", __name__)
 
-@jwt.unauthorized_loader(unauthorized_callback())
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    '''
+    If not a valid JWT token then redirect user to login page 
+    '''
+    try :
+        logging.debug("Not a valide JWT token please login back in ")
+        return redirect(url_for('user.login'))
+    except Exception as e :
+        logging.error("Error while redirecting user for re-login %s",e)
+   
 
 
 @movie_suggest.after_request
-@jwt_required(refresh_token())
+@jwt_required()
+def refresh_token(response):
+    '''
+    Refresh JWT token if expired or going to expire 
+    '''
+    try:
+        access_token = Auth.refresh_expiring_jwts()
+        if access_token:
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError) as e:
+        logging.error("Error while refreshing token %s ",e)
+        return response
 
 
 
